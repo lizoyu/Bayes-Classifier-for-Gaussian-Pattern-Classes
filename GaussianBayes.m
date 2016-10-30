de3fc828d38f892ab64437c1d14f254a747cd4e9
Chungyuen Li <lizoyu1@gmail.com>
2016-10-30 10:15:22 -0500

    Finish the function

--- GaussianBayes.m
+++ GaussianBayes.m
@@ -1,25 +1,46 @@
-function [ d21 ] = GaussianBayes( n, N, m1, m2 )
+function [ d21 ] = GaussianBayes( n, t1, t2 )
 % Bayes classifier for Gaussian pattern classes
-% - use training data to form the decision function
+% - use training data to form the decision function, assumed two training
+% data have the same amount.
 % input:
 % 	n: the amount of features (dimensions)
-%	N: the amount of pattern vectors (assumed both classes have the same amout of training data)
-%	m1: mean vector of class 1 (mean of the training data)
-%	m2: mean vector of class 2 (mean of the training data)
-C1 = zeros(n);
-C2 = C1(:);
-for i = 1:N
-    C1 = C1 + e1(:,i) * e1(:,i)';
-    C2 = C2 + e2(:,i) * e2(:,i)';
+%	t1: training data of class 1
+%	t2: training data of class 2
+
+% Adjust the size of training data
+[~, col1] = size(t1);
+[~, col2] = size(t2);
+if(col1 == n)
+    t1 = t1';
 end
+if(col2 == n)
+    t2 = t2';
+end
+[~, N] = size(t1);
+
+% Create the covariance matrices
+C1 = zeros(n); C2 = zeros(n);
+for i = 1:N
+    C1 = C1 + t1(:,i) * t1(:,i)';
+    C2 = C2 + t2(:,i) * t2(:,i)';
+end
+m1 = mean(t1,2); m2 = mean(t2,2);
 C1 = C1/N - m1 * m1';
 C2 = C2/N - m2 * m2';
 
-syms x1 x2 x3 x4 x5 x6
-x = [x1;x2;x3;x4;x5;x6];
-xt = [x1,x2,x3,x4,x5,x6];
-d1 = log(0.5) - 0.5*log(det(C1)) - 0.5*((xt-m1')*inv(C1)*(x-m1));
-d2 = log(0.5) - 0.5*log(det(C2)) - 0.5*((xt-m2')*inv(C2)*(x-m2));
-d21 = d2 - d1;
-vpa(simplify(d21),2)
+% Calculate the decision function and surface
+x = sym('x', [n 1], 'rational');
+if(det(C1)==0)
+    dC1 = 1;
+else
+    dC1 = det(C1);
+end
+if(det(C2)==0)
+    dC2 = 1;
+else
+    dC2 = det(C2);
+end
+d1 = log(0.5) - 0.5*log(dC1) - 0.5*((x-m1)'*pinv(C1)*(x-m1));
+d2 = log(0.5) - 0.5*log(dC2) - 0.5*((x-m2)'*pinv(C2)*(x-m2));
+d21 = vpa(simplify(d2 - d1),2);
 end
